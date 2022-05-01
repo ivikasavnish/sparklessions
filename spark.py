8ed134f1110be798a04edee6f5e1ce7adb5bfeb2
ivikasavnish <97333741+ivikasavnish@users.noreply.github.com>
2022-05-01 13:32:33 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -26,3 +26,17 @@ pd_counts = flight_counts.toPandas()
 
 # Print the head of pd_counts
 print(pd_counts.head())
+# Create pd_temp
+pd_temp = pd.DataFrame(np.random.random(10))
+
+# Create spark_temp from pd_temp
+spark_temp = spark.createDataFrame(pd_temp)
+
+# Examine the tables in the catalog
+print(spark.catalog.listTables())
+
+# Add spark_temp to the catalog
+spark_temp.createOrReplaceTempView("temp")
+
+# Examine the tables in the catalog again
+print(spark.catalog.listTables())
