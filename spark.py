b57c6a014142f56824a21fc576da52f2898913e1
ivikasavnish <97333741+ivikasavnish@users.noreply.github.com>
2022-05-01 13:34:02 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -40,3 +40,12 @@ spark_temp.createOrReplaceTempView("temp")
 
 # Examine the tables in the catalog again
 print(spark.catalog.listTables())
+
+# Don't change this file path
+file_path = "/usr/local/share/datasets/airports.csv"
+
+# Read in the airports data
+airports = spark.read.csv(file_path, header=True)
+
+# Show the data
+airports.show()
