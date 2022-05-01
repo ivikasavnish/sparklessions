ed4c9655c9448349aa86bdd825336353f91c8b5d
ivikasavnish <97333741+ivikasavnish@users.noreply.github.com>
2022-05-01 13:24:53 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -7,3 +7,11 @@ my_spark = SparkSession.builder.getOrCreate()
 print(my_spark)
 # Print the tables in the catalog
 print(spark.catalog.listTables())
+# Don't change this query
+query = "FROM flights SELECT * LIMIT 10"
+
+# Get the first 10 rows of flights
+flights10 = spark.sql(query)
+
+# Show the results
+flights10.show()
