f27084153db523c4c20fee6664310598c9650b0f
ivikasavnish <97333741+ivikasavnish@users.noreply.github.com>
2022-05-01 15:25:46 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -75,6 +75,18 @@ selected1 = flights.select("tailnum", "origin", "dest")
 # Select the second set of columns
 temp = flights.select(flights.origin, flights.dest, flights.carrier)
 
+# Import pyspark.sql.functions as F
+import pyspark.sql.functions as F
+
+# Group by month and dest
+by_month_dest = flights.groupBy("month","dest")
+
+# Average departure delay by month and destination
+by_month_dest.avg("dep_delay").show()
+
+# Standard deviation of departure delay
+by_month_dest.agg(F.stddev("dep_delay")).show()
+
 # Define first filter
 filterA = flights.origin == "SEA"
 
