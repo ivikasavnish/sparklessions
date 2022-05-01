9d72363e34c60081a02ec0dbc360d5e89bbc6532
ivikasavnish <97333741+ivikasavnish@users.noreply.github.com>
2022-05-01 13:26:10 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -15,3 +15,14 @@ flights10 = spark.sql(query)
 
 # Show the results
 flights10.show()
+# Don't change this query
+query = "SELECT origin, dest, COUNT(*) as N FROM flights GROUP BY origin, dest"
+
+# Run the query
+flight_counts = spark.sql(query)
+
+# Convert the results to a pandas DataFrame
+pd_counts = flight_counts.toPandas()
+
+# Print the head of pd_counts
+print(pd_counts.head())
