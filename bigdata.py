188b6e717359c0648a2dccef2c0fb96a07f7cf86
ivikasavnish <97333741+ivikasavnish@users.noreply.github.com>
2022-05-04 14:49:58 +0530

    updated source examples

--- bigdata.py
+++ bigdata.py
@@ -1,1 +1,79 @@
+# Print the version of SparkContext
+print("The version of Spark Context in the PySpark shell is", sc.version)
 
+# Print the Python version of SparkContext
+print("The Python version of Spark Context in the PySpark shell is", sc.pythonVer)
+
+# Print the master of SparkContext
+print("The master of Spark Context in the PySpark shell is", sc.master)
+
+# Create a Python list of numbers from 1 to 100 
+numb = range(1, 101)
+
+# Load the list into PySpark  
+spark_data = sc.parallelize(numb)
+
+# Load a local file into PySpark shell
+lines = sc.textFile(file_path)
+
+# Print my_list in the console
+print("Input list is", my_list)
+
+# Square all numbers in my_list
+squared_list_lambda = list(map(lambda x: x**2, my_list))
+
+# Print the result of the map function
+print("The squared numbers are", squared_list_lambda)
+
+# Print my_list2 in the console
+print("Input list is:", my_list2)
+
+# Filter numbers divisible by 10
+filtered_list = list(filter(lambda x: (x%10 == 0), my_list2))
+
+# Print the numbers divisible by 10
+print("Numbers divisible by 10 are:", filtered_list)
+
+# Create an RDD from a list of words
+RDD = sc.parallelize(["Spark", "is", "a", "framework", "for", "Big Data processing"])
+
+# Print out the type of the created object
+print("The type of RDD is", type(RDD))
+
+# Print the file_path
+print("The file_path is", file_path)
+
+# Create a fileRDD from file_path
+fileRDD = sc.textFile(file_path)
+
+# Check the type of fileRDD
+print("The file type of fileRDD is", type(fileRDD))
+
+# Check the number of partitions in fileRDD
+print("Number of partitions in fileRDD is", fileRDD.getNumPartitions())
+
+# Create a fileRDD_part from file_path with 5 partitions
+fileRDD_part = sc.textFile(file_path, minPartitions = 5)
+
+# Check the number of partitions in fileRDD_part
+print("Number of partitions in fileRDD_part is", fileRDD_part.getNumPartitions())
+
+# Create map() transformation to cube numbers
+cubedRDD = numbRDD.map(lambda x: x**3)
+
+# Collect the results
+numbers_all = cubedRDD.collect()
+
+# Print the numbers from numbers_all
+for numb in numbers_all:
+	print(numb)
+
+# Filter the fileRDD to select lines with Spark keyword
+fileRDD_filter = fileRDD.filter(lambda line: 'Spark' in line)
+
+# How many lines are there in fileRDD?
+print("The total number of lines with the keyword Spark is", fileRDD_filter.count())
+
+# Print the first four lines of fileRDD
+for line in fileRDD_filter.take(4): 
+  print(line)
